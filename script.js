d8d358f3b789757b4dfb2ecd1a5c84635710f239
Mahram Hossain <mhnazary2020@yahoo.com>
2023-05-03 23:52:24 +0430

    fix linters error

--- script.js
+++ script.js
@@ -59,10 +59,9 @@ class BookList {
 
 const myBookList = new BookList('#input-form', '#Title', '#Author', '#display-area');
 
+// Nabigation
 
-// Nabigation 
-
-const datetime = document.getElementById("datetime");
+const datetime = document.getElementById('datetime');
 
 setInterval(() => {
   const now = new Date();
@@ -71,42 +70,41 @@ setInterval(() => {
   const time = now.toLocaleTimeString();
   datetime.innerHTML = `${date} ${time}`;
 }, 1000);
+myBookList();
 
-let navList = document.getElementById('navList')
-let navAdd = document.getElementById('navAdd')
-let navContact = document.getElementById('navContact')
-let listHeader = document.getElementById('list')
-let addNewHeader = document.getElementById('add_new')
-let contactHeader = document.getElementById('contact')
-let bookList = document.getElementById('display-area')
-let Addbook = document.getElementById('input-form')
-let contact = document.getElementById('contact_sec')
+const navList = document.getElementById('navList');
+const navAdd = document.getElementById('navAdd');
+const navContact = document.getElementById('navContact');
+const listHeader = document.getElementById('list');
+const addNewHeader = document.getElementById('add_new');
+const contactHeader = document.getElementById('contact');
+const bookList = document.getElementById('display-area');
+const Addbook = document.getElementById('input-form');
+const contact = document.getElementById('contact_sec');
 
-navList.addEventListener('click',()=> {
-  listHeader.style.display ='inline'
-  bookList.style.display = 'inline'
-  addNewHeader.style.display ='none'
-  Addbook.style.display ='none';
-  contactHeader.style.display ='none'
-  contact.style.display ='none'
-})
+navList.addEventListener('click', () => {
+  listHeader.style.display = 'inline';
+  bookList.style.display = 'inline';
+  addNewHeader.style.display = 'none';
+  Addbook.style.display = 'none';
+  contactHeader.style.display = 'none';
+  contact.style.display = 'none';
+});
 
-navAdd.addEventListener('click',()=>{
-  listHeader.style.display ='none'
-  bookList.style.display = 'none'
-  addNewHeader.style.display ='inline'
-  Addbook.style.display ='flex';
-  contactHeader.style.display ='none'
-  contact.style.display ='none'
-})
+navAdd.addEventListener('click', () => {
+  listHeader.style.display = 'none';
+  bookList.style.display = 'none';
+  addNewHeader.style.display = 'inline';
+  Addbook.style.display = 'flex';
+  contactHeader.style.display = 'none';
+  contact.style.display = 'none';
+});
 
-navContact.addEventListener('click',()=>{
-  listHeader.style.display ='none'
-  bookList.style.display = 'none'
-  contactHeader.style.display ='inline'
-  contact.style.display= 'inline'
-  addNewHeader.style.display ='none'
-  Addbook.style.display ='none';
-})
-
-
+navContact.addEventListener('click', () => {
+  listHeader.style.display = 'none';
+  bookList.style.display = 'none';
+  contactHeader.style.display = 'inline';
+  contact.style.display = 'inline';
+  addNewHeader.style.display = 'none';
+  Addbook.style.display = 'none';
+});
